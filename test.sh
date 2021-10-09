0911b0f6461e70d1e74a964e4b915fad95ffd464
harshavn <linkern81@gmail.com>
2021-10-09 16:58:21 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,27 @@
-echo Hello World
+pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+        stage('Build') {
+            steps {
+                echo 'Building'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing'
+            }
+        }
+    }
+}
+
